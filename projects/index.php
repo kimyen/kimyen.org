51f2d87bfb705f2a0560b20a2d05cc861e2e8a2c
KimYen Truong <kimyen0816@gmail.com>
2014-09-26 20:52:51 +0000

    merge projects/index.html and projects/index.php

--- projects/index.php
+++ projects/index.php
@@ -10,6 +10,10 @@
 
         <!-- Bootstrap -->
         <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/portfolio-item.css" rel="stylesheet">
+
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -56,8 +60,75 @@
         
         <!-- Page Content -->
         <div class="container">
-
         
+            <!-- Portfolio Item Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Portfolio Item
+                        <small>Item Subheading</small>
+                    </h1>
+                </div>
+            </div>
+            <!-- /.row -->
+            
+            <!-- Portfolio Item Row -->
+            <div class="row">
+
+                <div class="col-md-8">
+                    <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+                </div>
+
+                <div class="col-md-4">
+                    <h3>Project Description</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                    <h3>Project Details</h3>
+                    <ul>
+                        <li>Lorem Ipsum</li>
+                        <li>Dolor Sit Amet</li>
+                        <li>Consectetur</li>
+                        <li>Adipiscing Elit</li>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- /.row -->
+
+            <!-- Related Projects Row -->
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <h3 class="page-header">Related Projects</h3>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <a href="#">
+                        <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                    </a>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <a href="#">
+                        <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                    </a>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <a href="#">
+                        <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                    </a>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <a href="#">
+                        <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                    </a>
+                </div>
+
+            </div>
+            <!-- /.row --> 
+            
+            
+
         </div>        
         <!-- Page Content -->
 
