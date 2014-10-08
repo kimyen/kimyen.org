3b3f6fb9e1a6973842ba41734447ac77bfaa3e19
KimYen Truong <kimyen0816@gmail.com>
2014-10-08 18:19:18 +0000

    style button

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ var dashboard = {
 	header : "Dashboard <small> Sage Bionetworks</small>",
 	description: "<p>Sage is popular with the Synapse software that helps scientists and cancer researchers to store, manage and share data. Dashboard is a web application that shows how users use Synapse.</p><p>I took requests from other developers and data scientists, and implemented new metrics for Dashboard. Also I’m working on building and maintaining a data warehouse on Amazon Redshift.</p>",
 	technology: "<ul><li>Eclipse Luna</li><li>Scala IDE for Eclipse</li><li>Redis</li><li>Play Framework</li></ul>",
-	source: "<button class='button'><a href='https://github.com/kimyen/dashboard'>Dashboard</a></button> <button class='button'><a href='https://github.com/kimyen/dashboard-ui'>Dashboard UI</a></button>",
+	source: "<button class='btn btn-default'><a href='https://github.com/kimyen/dashboard'>Dashboard</a></button> <button class='btn btn-default'><a href='https://github.com/kimyen/dashboard-ui'>Dashboard UI</a></button>",
     img: "/img/dashboard.png"
 };
 
@@ -18,7 +18,7 @@ var campusPath = {
 	header : "Campus Path <small> University of Washington </small>",
 	description: "<p>I developed a graph ADT and used Dijkstra's algorithm to find the shortest path from one location to another location at UW campus. I also implemented a Graphical User Interface for the application.</p> ",
 	technology: "<ul><li>Eclipse</li></ul>",
-	source: "<button class='button'><a href='https://github.com/kimyen/CampusPaths'>Campus Paths</a></button>",
+	source: "<button class='btn btn-default'><a href='https://github.com/kimyen/CampusPaths'>Campus Paths</a></button>",
     img: "/img/campuspath.png"
 };
 
@@ -26,7 +26,7 @@ var bj = {
 	header : "Blackjack <small> Bellevue College </small>",
 	description: "<p>Blackjack is a computer game that follow the rules of American Blackjack. The computer acts as a dealer. In the beginning, the player will be given a certain amount of money. Blackjack allows users to bet any amount of money within the amount they have and take back any amount of money within the amount they bet before a game begins... It can be addictive.</p>",
 	technology: "<ul><li>Eclipse</li></ul>",
-	source: "<button class='button'><a href='https://github.com/kimyen/Java'>Blackjack</a></button>",
+	source: "<button class='btn btn-default'><a href='https://github.com/kimyen/Java'>Blackjack</a></button>",
     img: "/img/bj.png"
 };
 
